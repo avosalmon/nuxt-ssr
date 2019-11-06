6cb425092e47c9606e3d024df1868b8b702d5bdc
avosalmon <avosalmon@gmail.com>
2019-11-06 16:39:25 +0800

    change subtitle

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,18 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        nuxt-ssr
-      </h1>
-      <h2 class="subtitle">
-        My amazing Nuxt.js project
-      </h2>
+      <h1 class="title">nuxt-ssr</h1>
+      <h2 class="subtitle">Heroku review app is amazing!!</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
+          >GitHub</a
         >
-          GitHub
-        </a>
       </div>
     </div>
   </div>
